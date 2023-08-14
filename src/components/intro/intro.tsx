4e64dc593239903116184013eb3842f27c8a1504
heinAstonish <heinplushtet@gmail.com>
2023-08-15 00:21:02 +0630

    change

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -50,7 +50,7 @@ export const Intro = ({ className }: IntroProps) => {
             </div>
             <p className={styles.p}>
                 {' '}
-                Mastering the Art of Designing Intuitive and Dynamic Web Interfaces for User Delight
+                MMastering the Art of Designing Intuitive and Dynamic Web Interfaces for User Delight
             </p>
 
             <motion.button
